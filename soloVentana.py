8d5020bbc50336cf355dc8cd086907353fd3bd80
David Calle <35737078+dcalled1@users.noreply.github.com>
2019-05-23 22:26:53 +0000

    A

--- soloVentana.py
+++ soloVentana.py
@@ -1,18 +1,62 @@
 from tkinter import *
+import time
 
-def abrirHistorial():
-    print("Historial")
+histAbierto=False
+devAbierto=False
+listAbierto=False
 
-def abrirLista():
-    print("Lista de materiales")
-
-def abrirDevoluciones():
-    print("Devoluciones")
+historial=None
+lista=None
+devoluciones=None
 
 vPrincipal = Tk()
 vPrincipal.geometry('150x230+200+200')
 vPrincipal.title('Sistema de prestamos')
 vPrincipal.config(bg = 'blue2')
+
+def closeHist():
+    global histAbierto, historial
+    histAbierto=False
+    historial.destroy()
+    
+def closeDev():
+    global devAbierto, devoluciones
+    devAbierto=False
+    devoluciones.destroy()
+    
+def closeList():
+    global listAbierto, lista
+    listAbierto=False
+    lista.destroy()
+
+def abrirHistorial():
+    global histAbierto, historial
+    if not histAbierto:
+        histAbierto=True
+        historial=Toplevel()
+        historial.geometry("800x800+500+100")
+        historial.title("Historial de prestamos")
+    historial.protocol("WM_DELETE_WINDOW", closeHist)
+
+def abrirLista():
+    global listAbierto, lista
+    if not listAbierto:
+        listAbierto=True
+        lista=Toplevel()
+        lista.geometry("800x800+500+100")
+        lista.title("Lista de materiales")
+    lista.protocol("WM_DELETE_WINDOW", closeList)
+
+def abrirDevoluciones():
+    global devAbierto, devoluciones
+    if not devAbierto:
+        devAbierto=True
+        devoluciones=Toplevel()
+        devoluciones.geometry("800x800+500+100")
+        devoluciones.title("Devoluciones pendientes")
+    devoluciones.protocol("WM_DELETE_WINDOW", closeDev)
+
+
 
 botonHistorial = Button(vPrincipal,
                         text='Historial',
@@ -35,6 +79,8 @@ botonDevoluciones = Button(vPrincipal,
 try:
     while 1:
         vPrincipal.update()
+        time.sleep(0.1)
 
 except TclError:
     print("Apagado exitoso")
+
