3bea62e6eebd17aa656ff3cbb8cf27eb31683775
David Calle Daza <dcalled1@eafit.edu.co>
2019-05-24 12:19:55 -0500

    a

---
+++ frames/inicio/FrameDevoluciones.py
@@ -0,0 +1,5 @@
+from tkinter import ttk
+
+class Devoluciones(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ frames/inicio/FrameMateriales.py
@@ -0,0 +1,5 @@
+from tkinter import ttk
+
+class Materiales(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
