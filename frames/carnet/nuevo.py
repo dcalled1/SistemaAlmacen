82c15e631247e98e90a30e0697a372d1d7b68082
David Calle <35737078+dcalled1@users.noreply.github.com>
2019-06-06 19:15:58 -0500

    a

--- frames/carnet/nuevo.py
+++ frames/carnet/nuevo.py
@@ -1,11 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+from data.modelos import *
 
 class NuevoCarnet(Toplevel):
     def __init__(self, id=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.title('Nuevo pedido')
 
-        ##self.conexion=Conexion()
+        self.conexion=Conexion()
 
         self.id=id
 
@@ -18,7 +20,7 @@ class NuevoCarnet(Toplevel):
         self.list.tag_bind("t", "<<TreeviewSelect>>",
                                self.itemSeleccionado)
 
-        #self.listar()
+        self.listar()
 
         self.list.pack(expand=True, fill=BOTH)
 
@@ -36,7 +38,31 @@ class NuevoCarnet(Toplevel):
         pass
 
     def confirmar(self):
-        pass
+        ids=self.list.selection()
+        ls=[]
+        for idmat in ids:
+            data=self.list.item(idmat)
+            dbobj=self.conexion.buscarMaterial(data.get('text'), data.get('values')[0])
+            ls.append(dbobj.get('_id'))
+        self.conexion.anadirPedido(self.id, ls)
+        self.master.historial.listar()
+        self.destroy()
+
+    def cancelar(self):
+        self.destroy()
+        
+    def listar(self):
+        docs=self.conexion.listarMateriales()
+        self.list.delete(*self.list.get_children())
+        for doc in docs:
+            if doc.get('cantidad') != 'NA':
+                if doc.get('cantidad') <= 0:
+                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'No disponible'))
+                else:
+                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'Disponible'))
+            else:
+                self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'Disponible'))
+
 
     def cancelar(self):
         pass
