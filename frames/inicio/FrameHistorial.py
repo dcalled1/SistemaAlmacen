8c385cf0bd0a13d23e762ed1ebb9cc38741f1a55
friosl <38120585+friosl@users.noreply.github.com>
2019-06-07 23:51:55 -0500

    Update FrameHistorial.py

--- frames/inicio/FrameHistorial.py
+++ frames/inicio/FrameHistorial.py
@@ -3,37 +3,36 @@ from tkinter import *
 from data.modelos import *
 import time
 
-class Historial(ttk.Frame):
+class Historial(ttk.Frame): 
 
-    def __init__(self, *args, **kwargs):
-        self.conexion= Conexion()
+    def __init__(self, *args, **kwargs):  
+        self.conexion= Conexion() #Instancia de la clase conexión, que conecta con la base de datos en mongo.
         super().__init__(*args, **kwargs)
 
-        self.list=ttk.Treeview(self, columns=("fecha"), selectmode=BROWSE)
+        self.list=ttk.Treeview(self, columns=("fecha"), selectmode=BROWSE) , # creando el TreeView para la ventana Historial
         self.list.heading("#0", text="Prestante")
         self.list.heading("fecha", text="Fecha")
         self.list.tag_bind("t", "<<TreeviewSelect>>",
                                self.itemSeleccionado)
-        self.listar()
+        self.listar()  
 
         self.list.pack(expand=True, fill=BOTH)
 
         self.pack(expand=True, fill=BOTH)
 
 
-    def itemSeleccionado(self, event):
-        idITEM=self.list.item(self.list.selection())
+    def itemSeleccionado(self, event): #Cuando se selecciona un item de historial, se abre una nueva ventana que contiene el nombre y la fecha en que se pidió
+        idITEM=self.list.item(self.list.selection()) 
         y=int(idITEM.get('values')[0].split('-')[0])
         m=int(idITEM.get('values')[0].split('-')[1])
         d=int(idITEM.get('values')[0].split('-')[2])
         start=datetime.datetime(y,m,d, 0, 0, 0)
         end=datetime.datetime(y,m,d, 23, 59, 59)
-        reg=self.conexion.buscarHistorial(idITEM.get('text'), {'$lt': end, '$gte': start})
-        print(reg)
-        obtener = obtenerHis(reg.get("_id"))
+        reg=self.conexion.buscarHistorial(idITEM.get('text'), {'$lt': end, '$gte': start}) #Obtener registro
+        obtener = obtenerHis(reg.get("_id")) #Crear la nueva ventana en la clase obtenerHis con la información del registro.
 
     def listar(self):
-        docs = self.conexion.listarHistorial()
+        docs = self.conexion.listarHistorial() #Busca en la base de datos y muestra lo que encuentre de Historial
         self.list.delete(*self.list.get_children())
         for doc in docs:
             self.list.insert('', END, text=doc.get('prestante'),
@@ -41,11 +40,11 @@ class Historial(ttk.Frame):
 
 
 
-class obtenerHis(Toplevel):
+class obtenerHis(Toplevel): #Segundo treeView de historial
     def __init__(self, id, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.id=id
-        self.conexion= Conexion()
+        self.conexion= Conexion() 
         self.title("Historial")
         self.config()
         self.geometry("400x400+300+100")
@@ -53,16 +52,14 @@ class obtenerHis(Toplevel):
         self.list.heading("#0", text="Codigo")
         self.list.heading("nombre", text="Nombre")
 
-        self.listarMat()
+        self.listarMat() #Llama al método listar Materiales
         self.list.pack(expand=True, fill=BOTH)
-        ##arr=self.list.item(self.list.selection())
-
 
     def listarMat(self):
-        registro = self.conexion.buscarHistxID(self.id)
-        materiales=registro.get('materiales')
+        registro = self.conexion.buscarHistxID(self.id) #Buscamos los materiales con el id de la persona y la hora. Funciona como diccionario
+        materiales=registro.get('materiales') 
         self.list.delete(*self.list.get_children())
-        for i in materiales:
+        for i in materiales: #Para recorrer cuando se presten varios objetos a la vez
             mat=self.conexion.buscarMatxID(i)
             self.list.insert('', END, text= mat.get('codigo'),
                              values=(mat.get('nombre')), tags=("t",))
