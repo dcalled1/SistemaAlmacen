bf0ed083e0461e6fe4efdf9b43fb163d45dca56a
friosl <38120585+friosl@users.noreply.github.com>
2019-06-08 02:07:31 -0500

    Update nuevo.py

--- frames/carnet/nuevo.py
+++ frames/carnet/nuevo.py
@@ -7,11 +7,11 @@ class NuevoCarnet(Toplevel):
         super().__init__(*args, **kwargs)
         self.title('Nuevo pedido')
 
-        self.conexion=Conexion()
+        self.conexion=Conexion() #Instancia clase conexión
 
         self.id=id
 
-        self.list=ttk.Treeview(self, columns=("nombre", "disponibilidad"))
+        self.list=ttk.Treeview(self, columns=("nombre", "disponibilidad")) #Crear treeview
 
         self.list.heading("#0", text="Código")
         self.list.heading("nombre", text="Nombre")
@@ -24,11 +24,11 @@ class NuevoCarnet(Toplevel):
 
         self.list.pack(expand=True, fill=BOTH)
 
-        self.aceptar=Button(self, text="Confirmar selección",
+        self.aceptar=Button(self, text="Confirmar selección", #Botón confirmar
                             command=self.confirmar)
         self.aceptar.pack(side=RIGHT)
 
-        self.cancel=Button(self, text="Cancelar",
+        self.cancel=Button(self, text="Cancelar", #Botón cancelar acción
                              command=self.cancelar)
         self.cancel.pack(side=RIGHT)
 
@@ -38,30 +38,30 @@ class NuevoCarnet(Toplevel):
         pass
 
     def confirmar(self):
-        ids=self.list.selection()
+        ids=self.list.selection() #Obtener los materiales seleccionados
         ls=[]
         for idmat in ids:
             data=self.list.item(idmat)
-            dbobj=self.conexion.buscarMaterial(data.get('text'), data.get('values')[0])
-            ls.append(dbobj.get('_id'))
-        self.conexion.anadirPedido(self.id, ls)
-        self.master.historial.listar()
-        self.master.devoluciones.listar()
+            dbobj=self.conexion.buscarMaterial(data.get('text'), data.get('values')[0]) #Buscar en la base de datos los materiales.
+            ls.append(dbobj.get('_id')) #Agregar el id al arreglo
+        self.conexion.anadirPedido(self.id, ls) #Añadir nuevo material prestado a la base de datos.
+        self.master.historial.listar() #Actualizar historial
+        self.master.devoluciones.listar() #Actualizar devoluciones
         self.destroy()
 
     def cancelar(self):
         self.destroy()
         
     def listar(self):
-        docs=self.conexion.listarMateriales()
+        docs=self.conexion.listarMateriales() #Mostrar los materiales que hay en la base de datos.
         self.list.delete(*self.list.get_children())
         for doc in docs:
             cantidad=int(doc.get('cantidad'))
             if cantidad != -1:
                 if cantidad == 0:
-                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'No disponible'))
+                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'No disponible')) #Mostrar en el treeview si la cantidad no aplica
                 else:
-                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'Disponible'))
+                    self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'Disponible')) #Mostrar en el treeview si existe
             else:
                 self.list.insert('', END, text=doc.get('codigo'), values=(doc.get('nombre'), 'Disponible'))
 
