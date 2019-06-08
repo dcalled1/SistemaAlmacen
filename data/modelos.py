d74eab125343368bbe7c6086394a6b389edc1efc
friosl <38120585+friosl@users.noreply.github.com>
2019-06-08 02:19:27 -0500

    Update modelos.py

--- data/modelos.py
+++ data/modelos.py
@@ -3,18 +3,18 @@ import datetime
 
 
 class Conexion:
-    uri = 'mongodb+srv://almacen:almacenPass@pandoracluster-mibdw.azure.mongodb.net/AlmacenDB?retryWrites=true'
-    dbname = 'AlmacenDB'
+    uri = 'mongodb+srv://almacen:almacenPass@pandoracluster-mibdw.azure.mongodb.net/AlmacenDB?retryWrites=true' #link Conexion
+    dbname = 'AlmacenDB' 
 
     def __init__(self):
 
-        self.client = MongoClient()
+        self.client = MongoClient() #Inicializando mongo
 
-        self.db = self.client[self.dbname]
+        self.db = self.client[self.dbname] 
 
-        self.material = self.db['Material']
-        self.historial = self.db['Historial']
-        self.devolucion = self.db['Devolucion']
+        self.material = self.db['Material']       # tabla material
+        self.historial = self.db['Historial']     # tabla historial
+        self.devolucion = self.db['Devolucion']   # tabla devolucion
 
     def cerrar(self):
         self.client.close()
@@ -24,15 +24,14 @@ class Conexion:
             'codigo': codigo,
             'nombre': nombre,
             'cantidad': cantidad
-        }
-        print(post)
-        self.material.insert(post)
+        } #Diccionario con la información del material
+        self.material.insert(post) #Agregar a la tabla material
 
     def listarMateriales(self):
-        return self.material.find()
+        return self.material.find() #Buscar en la BD
         
     def quitarMaterial(self, codigo, nombre):
-        ids=self.material.remove({'codigo': codigo, 'nombre': nombre})
+        ids=self.material.remove({'codigo': codigo, 'nombre': nombre}) #Quitarlo de la BD
 
     def agregarHistorial(self,codigo,cantidad,nombre):
         post = {
@@ -40,48 +39,48 @@ class Conexion:
             'cantidad':cantidad,
             'nombre':nombre
         }
-        self.material.find_one(post)
+        self.material.find_one(post) #Buscar para mostrar en el historial
         #Ese nombre se pondrá en el otro frame
         #obtener la fecha
 
     def listarHistorial(self):
-        return self.historial.find()
+        return self.historial.find() #Retorna el historial
 
     def buscarHistorial(self, prestante, fecha):
-        return self.historial.find_one({'prestante': int(prestante), 'fecha': fecha})
+        return self.historial.find_one({'prestante': int(prestante), 'fecha': fecha}) #Retorna el diccionario específico en historial con el id y la fecha
 
     def buscarMatxID(self, id):
-        return self.material.find_one({'_id': id})
+        return self.material.find_one({'_id': id}) #Retorna el diccionario con el id
 
     def buscarHistxID(self, id):
-        return self.historial.find_one({'_id': id})
+        return self.historial.find_one({'_id': id}) #Retorna el diccionario con el ID
 
     def buscarDevxID(self, id):
-        return self.devolucion.find_one({'_id': id})
+        return self.devolucion.find_one({'_id': id}) #Retorna el diccionario con el ID
 
     def listarDevoluciones(self):
-        return self.devolucion.find()
+        return self.devolucion.find() #Busca en devoluciones
 
     def buscarDevoluciones(self, prestante, fecha):
-        return self.devolucion.find_one({'prestante': prestante, 'fecha': fecha})
+        return self.devolucion.find_one({'prestante': prestante, 'fecha': fecha}) #Retorna el diccionario específico en devoluciones con el id y la fecha
         
     def anadirPedido(self, prestante, materiales):
         print(materiales)
-        post={'prestante': prestante, 'materiales': materiales, 'fecha': datetime.datetime.now()}
-        self.historial.insert(post)
-        self.devolucion.insert(post)
+        post={'prestante': prestante, 'materiales': materiales, 'fecha': datetime.datetime.now()} #Añade la fecha actual al pedido
+        self.historial.insert(post) #Agrega a historial
+        self.devolucion.insert(post) #Agrega a devolucion
 		
     def buscarMaterial(self, codigo, nombre):
-        return self.material.find_one({'codigo': codigo , 'nombre': nombre})
+        return self.material.find_one({'codigo': codigo , 'nombre': nombre}) #Retorna el diccionario de material específico
 
     def checarDevolucion(self, id):
-        if self.devolucion.find({'prestante': id}).count() == 0:
+        if self.devolucion.find({'prestante': id}).count() == 0: #Mirar si existe la devolución
             return False
         else:
             return True
 
     def listarMatPrestante(self, prest):
-        docs = self.devolucion.find({'prestante': prest})
+        docs = self.devolucion.find({'prestante': prest}) #Materiales del prestante
         materiales=[]
         for doc in docs:
             materiales.extend(doc.get('materiales'))
@@ -89,8 +88,3 @@ class Conexion:
 
     def devolverMaterial(self, prest):
         ids=self.devolucion.remove({'prestante': prest})
-
-
-
-
-
